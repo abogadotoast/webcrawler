85729df463505a984c63e041dc7b02e421dbec33
abogadotoast <gugraves@gmail.com>
2024-03-13 19:54:12 -0700

    Initial commit.

---
+++ WebCrawlerUnitTests/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace WebCrawlerUnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
